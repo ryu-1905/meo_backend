713e4b16a75ef676e1bb5be3a233ce10880e4a4b
Ryu-1905 <hungg562002@gmail.com>
2025-07-25 20:24:06 +0900

    create project

---
+++ src/main/java/ryu/meo/MeoApplication.java
@@ -0,0 +1,13 @@
+package ryu.meo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ryu/meo/MeoApplicationTests.java
@@ -0,0 +1,13 @@
+package ryu.meo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
