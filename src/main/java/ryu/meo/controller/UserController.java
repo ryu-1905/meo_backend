afb196ea6f7ec07226227d9f81f9911be360cfb4
Ryu-1905 <hungg562002@gmail.com>
2025-08-05 15:55:37 +0900

    feat: implement Note management functionality with CRUD operations

--- src/main/java/ryu/meo/controller/UserController.java
+++ src/main/java/ryu/meo/controller/UserController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Modification Logs:
@@ -39,73 +38,73 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserController {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    String issuerUri;
+        @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+        String issuerUri;
 
-    @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
-    String clientId;
+        @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
+        String clientId;
 
-    @Value("${spring.security.oauth2.client.registration.keycloak.client-secret}")
-    String clientSecret;
+        @Value("${spring.security.oauth2.client.registration.keycloak.client-secret}")
+        String clientSecret;
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.admin-uri}")
-    String adminUri;
+        @Value("${spring.security.oauth2.resourceserver.jwt.admin-uri}")
+        String adminUri;
 
-    final RestClient restClient = RestClient.create();
+        final RestClient restClient = RestClient.create();
 
-    @GetMapping("/info")
-    public ResponseEntity<String> getUserInfo(@AuthenticationPrincipal Jwt jwt) {
+        @GetMapping("/info")
+        public ResponseEntity<String> getUserInfo(@AuthenticationPrincipal Jwt jwt) {
 
-        return ResponseEntity.ok(restClient.get()
-                .uri(issuerUri + "/protocol/openid-connect/userinfo")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwt.getTokenValue())
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .body(String.class));
-    }
+                return ResponseEntity.ok(restClient.get()
+                                .uri(issuerUri + "/protocol/openid-connect/userinfo")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwt.getTokenValue())
+                                .accept(MediaType.APPLICATION_JSON)
+                                .retrieve()
+                                .body(String.class));
+        }
 
-    @PostMapping("/access-token")
-    public ResponseEntity<String> getAccessToken(@RequestBody RefreshTokenRequestDTO refreshTokenDTO) {
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "refresh_token");
-        params.add("refresh_token", refreshTokenDTO.getRefreshToken());
-        params.add("client_id", clientId);
-        params.add("client_secret", clientSecret);
+        @PostMapping("/access-token")
+        public ResponseEntity<String> getAccessToken(@RequestBody RefreshTokenRequestDTO refreshTokenDTO) {
+                MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+                params.add("grant_type", "refresh_token");
+                params.add("refresh_token", refreshTokenDTO.getRefreshToken());
+                params.add("client_id", clientId);
+                params.add("client_secret", clientSecret);
 
-        return ResponseEntity.ok(restClient.post()
-                .uri(issuerUri + "/protocol/openid-connect/token")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(params)
-                .retrieve()
-                .body(String.class));
-    }
+                return ResponseEntity.ok(restClient.post()
+                                .uri(issuerUri + "/protocol/openid-connect/token")
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .body(params)
+                                .retrieve()
+                                .body(String.class));
+        }
 
-    @PutMapping("/update")
-    public ResponseEntity<String> updateUserInfo(@AuthenticationPrincipal Jwt jwt,
-            @RequestBody UpdateUserInfoRequestDTO updateUserInfoRequestDTO) {
+        @PutMapping("/update")
+        public ResponseEntity<String> updateUserInfo(@AuthenticationPrincipal Jwt jwt,
+                        @RequestBody UpdateUserInfoRequestDTO updateUserInfoRequestDTO) {
 
-        MultiValueMap<String, String> params1 = new LinkedMultiValueMap<>();
-        params1.add("grant_type", "client_credentials");
-        params1.add("client_id", clientId);
-        params1.add("client_secret", clientSecret);
+                MultiValueMap<String, String> params1 = new LinkedMultiValueMap<>();
+                params1.add("grant_type", "client_credentials");
+                params1.add("client_id", clientId);
+                params1.add("client_secret", clientSecret);
 
-        String adminAccessToken = restClient.post()
-                .uri(issuerUri + "/protocol/openid-connect/token")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(params1)
-                .retrieve()
-                .body(AdminAccessTokenResponse.class).getAccessToken();
+                String adminAccessToken = restClient.post()
+                                .uri(issuerUri + "/protocol/openid-connect/token")
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .body(params1)
+                                .retrieve()
+                                .body(AdminAccessTokenResponse.class).getAccessToken();
 
-        Map<String, Object> params2 = new HashMap<>();
-        params2.put("email", updateUserInfoRequestDTO.getEmail());
-        params2.put("attributes", Map.of("nickName", updateUserInfoRequestDTO.getNickName()));
+                Map<String, Object> params2 = new HashMap<>();
+                params2.put("email", updateUserInfoRequestDTO.getEmail());
+                params2.put("attributes", Map.of("nickName", updateUserInfoRequestDTO.getNickName()));
 
-        restClient.put()
-                .uri(adminUri + "/" + jwt.getSubject())
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + adminAccessToken)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(params2)
-                .retrieve().toBodilessEntity();
-        return ResponseEntity.ok("User info updated");
-    }
+                restClient.put()
+                                .uri(adminUri + "/" + jwt.getSubject())
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + adminAccessToken)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(params2)
+                                .retrieve().toBodilessEntity();
+                return ResponseEntity.ok("User info updated");
+        }
 }
---
+++ src/main/java/ryu/meo/dto/request/NoteRequestDTO.java
@@ -0,0 +1,16 @@
+package ryu.meo.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+public class NoteRequestDTO {
+    Long id;
+
+    String title;
+    String content;
+}
